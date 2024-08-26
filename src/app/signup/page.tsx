67c2345c579c4a1cd088562942a8a4c75c060d56
agent19music <seanmotanya@gmail.com>
2024-08-26 18:53:39 +0300

    Add signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,108 @@
+"use client"
+import Link from "next/link"
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Icons } from "@/components/icons"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+const CreateAccount: React.FC = () => {
+  const [category, setCategory] = useState("open")
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardDescription>
+            Enter your information to create an account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid grid-cols-2 gap-8 p-7">
+              <div className="grid gap-2">
+                <Label htmlFor="first-name">First name</Label>
+                <Input id="first-name" placeholder="Max" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="last-name">Last name</Label>
+                <Input id="last-name" placeholder="Robinson" required />
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="email@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+  <Label htmlFor="category">Course</Label>
+  <DropdownMenu>
+    <DropdownMenuTrigger asChild>
+      <Button variant="outline">{category ? category : "Open"}</Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent className="w-56">
+      <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuRadioGroup value={category} onValueChange={setCategory}>
+        <DropdownMenuRadioItem value="sw">Software Development</DropdownMenuRadioItem>
+        <DropdownMenuRadioItem value="ui/ux">UI/UX Design</DropdownMenuRadioItem>
+        <DropdownMenuRadioItem value="ds">Data Science</DropdownMenuRadioItem>
+        <DropdownMenuRadioItem value="cybersec">Cyber Security</DropdownMenuRadioItem>
+      </DropdownMenuRadioGroup>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</div>
+            <div className="grid gap-2">
+              <Label htmlFor="username">username</Label>
+              <Input
+                id="username"
+                type="text"
+                placeholder="johndoe23"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+            <Button type="submit" className="w-full">
+              Create an account
+            </Button>
+            <Button variant="outline" className="w-full">
+              <Icons.google className="size-4 mr-2"/>
+              Sign up with Google
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?{" "}
+            <Link href="/login" className="underline">
+              Sign in
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default CreateAccount
