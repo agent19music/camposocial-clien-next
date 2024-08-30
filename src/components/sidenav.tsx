95d7d4c30897df437a9cd999cbbd0dd81863f91c
agent19music <seanmotanya@gmail.com>
2024-08-30 05:50:54 +0300

    Update sidenav component

--- src/components/sidenav.tsx
+++ src/components/sidenav.tsx
@@ -4,6 +4,8 @@ import { Bell, Package2 } from "lucide-react";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"; 
 import { Button } from "./ui/button";
 import { Badge } from "./ui/badge";
+import { Home,Calendar, PartyPopper, ShoppingCart, Package, Users, LineChart } from "lucide-react";
+
 
 // Define the type for the link objects
 interface NavLink {
@@ -24,7 +26,7 @@ const SideNav: FC<SideNavProps> = ({ links }) => {
       <div className="flex h-full max-h-screen flex-col gap-2">
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/" className="flex items-center gap-2 font-semibold">
-            <Package2 className="h-6 w-6" />
+            
             <span>Camposocial</span>
           </Link>
           <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
@@ -51,8 +53,50 @@ const SideNav: FC<SideNavProps> = ({ links }) => {
             ))}
           </nav>
         </div>
+         {/*<div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Home className="h-4 w-4" />
+                Yap
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <ShoppingCart className="h-4 w-4" />
+                Events
+                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                  6
+                </Badge>
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+              >
+                <Package className="h-4 w-4" />
+                Marketplace{" "}
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Users className="h-4 w-4" />
+                Users
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <LineChart className="h-4 w-4" />
+                Funtimes
+              </Link>
+            </nav>
+          </div>*/}
         <div className="mt-auto p-4">
-          <Card>
+         {/* <Card>
             <CardHeader className="p-2 pt-0 md:p-4">
               <CardTitle>Upgrade to Pro</CardTitle>
               <CardDescription>
@@ -64,7 +108,7 @@ const SideNav: FC<SideNavProps> = ({ links }) => {
                 Upgrade
               </Button>
             </CardContent>
-          </Card>
+          </Card>*/}
         </div>
       </div>
     </div>
