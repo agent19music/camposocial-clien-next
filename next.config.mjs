131f17c67da4e95c3bb27e9b8127919e8f9062ad
agent19music <seanmotanya@gmail.com>
2024-10-05 12:02:00 +0300

    Update configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: '6eb9d92e84a9ebc29025971e4cef544e.r2.cloudflarestorage.com', // Cloudflare R2 hostname
+            port: '',
+            pathname: '/**', // This allows all paths under this hostname
+          },
+          {
+            protocol: 'https',
+            hostname: 'pub-c6a134c8e1fd4881a475bf80bc0717ba.r2.dev', // Cloudflare R2 hostname
+            port: '',
+            pathname: '/**', // This allows all paths under this hostname
+          },
+        ],
+      },
+};
 
 export default nextConfig;
