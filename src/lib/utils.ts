38594a4bbfcd2e1a59b6daa38aac1774d382dbe3
Sean <seanmotanya@gmail.com>
2024-10-12 00:00:00 +0400

    Update utils file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: string | Date) {
+  const d = new Date(date)
+  return new Intl.DateTimeFormat('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric'
+  }).format(d)
+}
