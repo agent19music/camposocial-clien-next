9891ed1cec52f4048715b0b73301e6c79fe65278
Sean <seanmotanya@gmail.com>
2025-02-05 15:53:53 +0300

    Add reset password page

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,10 @@
+import { ResetPassword } from "@/components/resetpassword/ResetPassword"
+
+export default function ResetPasswordPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <ResetPassword />
+    </div>
+  )
+}
+
